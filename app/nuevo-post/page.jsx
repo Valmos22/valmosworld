11836e1cc97106cae71e73130825a94426f83a31
Valmos22 <hugovalderrama5@gmail.com>
2025-05-18 20:09:53 -0500

    Primer commit

--- app/nuevo-post/page.jsx
+++ app/nuevo-post/page.jsx
@@ -23,16 +23,6 @@ export default function NuevoPost() {
 
     const handleSubmit = async e => {
         e.preventDefault();
-
-        // const response = await fetch('/api/guardar-post', {
-        //     method: 'POST',
-        //     headers: { 'Content-Type': 'application/json' },
-        //     body: JSON.stringify(form),
-        // });
-
-        // const data = await response.json();
-        // alert(data.message);
-
         console.log(form)
         const posts = JSON.parse(localStorage.getItem('posts')) || [];
         posts.push(form);
