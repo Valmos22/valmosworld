3c347eaf218a60886824e5733ecdd072bd183161
Valmos22 <hugovalderrama5@gmail.com>
2025-05-22 16:21:25 -0500

    update: Se agrega script de google ads

--- app/layout.js
+++ app/layout.js
@@ -49,6 +49,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
+      <head>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2528634736858471"
+          crossorigin="anonymous">
+
+        </script>
+      </head>
       <body id="body">
         <Providers>
           <Navbar />
